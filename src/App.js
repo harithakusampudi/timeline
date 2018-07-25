a06edc693f892094a05c21c3794847a75813d03d
Unknown <harithakusampudi89@gmail.com>
2018-07-25 17:07:37 +0530

    Nested groups with toggle

--- src/App.js
+++ src/App.js
@@ -5,85 +5,61 @@ import './App.css'
 import data from './data'
 import ThermalMap from './thermalMap'
 
-const groupsExample = {
-  groups: [],
-  items: [{
-    id: 'A', content: 'Group A', start: '2014-01-16', end: '2014-01-22', type: 'background', group: 11},
-    {id: 'B', content: 'Group B', start: '2014-01-23', end: '2014-01-26', type: 'background', group: 12},
-    {id: 'C', content: 'Group C', start: '2014-01-27', end: '2014-02-03', type: 'background'}, // no group
-    {id: 'D', content: 'Group D', start: '2014-01-14', end: '2014-01-20', type: 'background', },
-    {id: 1, content: 'item 1', start: '2014-01-30', group: 10,subgroup:1},
-    {id: 2, content: 'item 2', start: '2014-01-18', group: 11,subgroup:2},
-    {id: 3, content: 'item 3', start: '2014-01-21', group: 12},
-    {id: 4, content: 'item 4', start: '2014-01-17', end: '2014-01-21', group:13},
-    {id: 5, content: 'item 5', start: '2014-01-28', type:'point', group: 14},
-    {id: 6, content: 'item 6', start: '2014-01-25', group: 12}
-  ],
-  options: {
-    groupOrder: 'content' // groupOrder can be a property name or a sorting function,
-  }
-}
-
-const now = moment()
-  .minutes(0)
-  .seconds(0)
-  .milliseconds(0)
-const groupCount = 5
-const itemCount = 20
-
-// create a data set with groups
-const names = ['Truck 1', 'Truck 2', 'Truck 3', 'Truck 4', 'Truck 5']
-for (let g = 0; g < groupCount; g++) {
-  groupsExample.groups.push({ id: g, content: names[g] })
-}
-for(let i=0;i<groupCount;i++){
-  var nestedGroups=[]
-  nestedGroups.push(i)
-  console.log("nestedgroups",nestedGroups)
-  groupsExample.groups.push({
-    id:10+i,
-    content:'Bullard'+i,
-    nestedGroups:nestedGroups,
-    showNested:false
-
-  })
-}
-console.log("groups");
-
-// create a dataset with items
-// for (let i = 0; i < itemCount; i++) {
-//   groupsExample.items.push({
-//     id: i,
-//     group: (i) % 5,
-//     content:
-//       '<span style="color:#000;font-size:12px">Order ' +
-//       i + '</span>',
-//     start: new Date(2010, 7 + i, 15),
-//     end:new Date(2011,7+i,15),
-//     type: 'background'
-//   })
-// }
-
 class App extends Component {
   constructor (props) {
     super(props)
-
     this.state = {
       selectedIds: [],
-      selectedTruck: 'none'
+      selectedTruck: 'none',
+      show:true,
+      groupsExample : {
+        groups: [{
+          content: "bollard 1",
+          id: 1
+        },
+        {
+          content: "bollard 2",
+          id: 2
+        },
+        {
+          content: "bollard 3",
+          id: 3
+        },
+         {
+          id: 10,
+          nestedGroups: [1,2],
+          content: "Berth 1",
+          showNested:true
+        },{
+          id: 11,
+          nestedGroups: [3],
+          content: "Berth 2",
+          showNested:true
+        }],
+        items: [{  id: 'A', content: 'Group A', start: '2014-01-16', end: '2014-01-22', type: 'range', group: 11},
+        {id: 'B', content: 'Group B', start: '2014-01-23', end: '2014-01-26', type: 'range',group:10},
+        {id: 'C', content: 'order 1', start: '2014-01-23', end: '2014-01-26', type: 'box',group:10,subgroup:2},
+        {id: 'D', content: 'order 2', start: '2014-01-23', end: '2014-01-26', type: 'box',group:11,subgroup:3},
+        {id: 'E', content: 'order 3', start: '2014-01-23', end: '2014-01-26', type: 'box',group:10,subgroup:1}],
+        options: {
+          groupOrder: 'content' // groupOrder can be a property name or a sorting function,
+        }
+      }
     }
     this.clickHandler = this.clickHandler.bind(this)
   }
 
   render () {
+
+    console.log("dhfgdsjfg",this.state.groupsExample);
     var options = {
       width: '100%',
       margin: {
         item: 4
       },
       orientation: {
-        axis: 'top'
-      },
+        axis: 'both'
+      }, 
       editable: {
         add: true,
         updateTime: true,
@@ -99,9 +75,10 @@ class App extends Component {
     }
     return (
       <div className='App'>
-        <p>Berth   1</p>
+        <p>Vessel 1</p>
+        <button onClick={this.handleToggle.bind(this)} style={{display:'flex',float:'left'}}>{this.state.show?'-':'+'}</button>
         <Timeline
-          {...groupsExample}
+          {...this.state.groupsExample}
           options={options}
           clickHandler={this.clickHandler.bind(this)}
           selection={this.state.selectedIds}
@@ -114,12 +91,29 @@ class App extends Component {
   onMove (props) {
     console.log('ghjfjdkghfdgk', props)
   }
+  handleToggle(){
+    console.log("show",this.state.show );
+    this.setState({show:!this.state.show})
+    this.state.groupsExample.groups.find((group)=>{
+      if(group.id>=10)
+      console.log("group.showNested",group.showNested);
+      if(this.state.show)
+       group.showNested=false
+       else
+       group.showNested=true
+    })
+  }
+
   clickHandler (props) {
     const { group } = props
-    console.log('group', group)
     var no = group + 1
-    if (this.state.selectedTruck !== 'Truck' + no) { this.setState({selectedTruck: 'Truck' + no}) }
+    const groups =this.state.groupsExample.groups
+    var index = groups.map(function(e) { return e.id; }).indexOf(props.group);
+      var show=groups[index].showNested
+      groups[index].showNested=!show
+      this.setState({groups})
+      if (this.state.selectedTruck !== 'Truck' + no) { this.setState({selectedTruck: 'Truck' + no}) }
   }
 }
 
-export default App
+export default App  
